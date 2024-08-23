3b37e19bec5baab9c1dc254b5889275cf8732beb
Guido Lemeer <guido.lemeer@gmail.com>
2024-08-22 20:27:26 -0400

    Set Up Application - gems and node modules

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { plugins } = require("./postcss.config");
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
+}
